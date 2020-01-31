24ac2ec7d9e59f7fe889d21df0fc0dd8633f39d4
julio hernandez <hernandezjulio58@ymail.com>
2020-01-31 15:38:12 -0800

    Updates

---
+++ precache-manifest.d9f370f175f3177173610f66a5493e9c.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bf7dc8befcf5eb0562930e2ea953f8e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "5e27bbf2a5d583194298",
+    "url": "/portfolio/static/css/main.9309ee46.chunk.css"
+  },
+  {
+    "revision": "1313669212e82f1b99b0",
+    "url": "/portfolio/static/js/2.bcfad818.chunk.js"
+  },
+  {
+    "revision": "5e27bbf2a5d583194298",
+    "url": "/portfolio/static/js/main.8818c240.chunk.js"
+  },
+  {
+    "revision": "791f997dc0acbb20fb93",
+    "url": "/portfolio/static/js/runtime-main.021c856b.js"
+  },
+  {
+    "revision": "e0add6d5470bb45c680cd89298257ad2",
+    "url": "/portfolio/static/media/SpacedRep.e0add6d5.png"
+  },
+  {
+    "revision": "e6c6337e136e3ae087952bcf7861e1d0",
+    "url": "/portfolio/static/media/The Office Quiz.e6c6337e.png"
+  },
+  {
+    "revision": "97447636656a81f3f309c815437e1b6c",
+    "url": "/portfolio/static/media/bookmarker.97447636.png"
+  },
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "/portfolio/static/media/code.76d344f0.svg"
+  },
+  {
+    "revision": "595ce2f326ad1e8905d62f124ff1fec9",
+    "url": "/portfolio/static/media/email.595ce2f3.svg"
+  },
+  {
+    "revision": "57f856437908877fb031c6b53ef2a329",
+    "url": "/portfolio/static/media/email1.57f85643.svg"
+  },
+  {
+    "revision": "364b6f4a4c3d5e427146468d5ceab0a8",
+    "url": "/portfolio/static/media/github1.364b6f4a.svg"
+  },
+  {
+    "revision": "aeaf261574a1bcdf78eb5bdd626c0753",
+    "url": "/portfolio/static/media/home.aeaf2615.svg"
+  },
+  {
+    "revision": "2044f92f6f3b60bc4224afb9ef3b225e",
+    "url": "/portfolio/static/media/identification.2044f92f.svg"
+  },
+  {
+    "revision": "9e36c8fd15d6f93ed6a45f9484746e8f",
+    "url": "/portfolio/static/media/linkedin.9e36c8fd.svg"
+  },
+  {
+    "revision": "4b9d67074bdea08052fb1146e4779e9d",
+    "url": "/portfolio/static/media/linkedin1.4b9d6707.svg"
+  },
+  {
+    "revision": "684f8168807aded6a3e5007394d1e54c",
+    "url": "/portfolio/static/media/liveList.684f8168.png"
+  },
+  {
+    "revision": "f362f9c2bffb2437646193bd872cf80b",
+    "url": "/portfolio/static/media/livesite.f362f9c2.jpg"
+  },
+  {
+    "revision": "e1e22107121e8033306e9204887c6dc2",
+    "url": "/portfolio/static/media/particles.e1e22107.mp4"
+  },
+  {
+    "revision": "4f13404aa04805594ae255d9e73df9c8",
+    "url": "/portfolio/static/media/phone.4f13404a.png"
+  },
+  {
+    "revision": "38469b6adf4f09e975e2e03856495e9d",
+    "url": "/portfolio/static/media/phone1.38469b6a.jpg"
+  },
+  {
+    "revision": "ea9e0a9b38f79e2af10fe20f56035f77",
+    "url": "/portfolio/static/media/ticTacToe.ea9e0a9b.png"
+  },
+  {
+    "revision": "02fc4612e1f157389c6b6ecd29ee226d",
+    "url": "/portfolio/static/media/web-programming.02fc4612.svg"
+  }
+]);
